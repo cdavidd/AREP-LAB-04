ae4f20c65aa84233a4a468e4890ae8f0676621a9
cdavidd <cdavidlopez@hotmail.com>
2020-02-07 11:24:38 -0500

    inicial

---
+++ src/main/java/edu/escuelaing/arep/app/Server.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arep.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
